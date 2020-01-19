813a40f198754d621126f23c2f753c1bf0f6291d
ptoka <plutojts@126.com>
2020-01-19 17:00:44 +0800

    'swiper'

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h2>主页</h2>
+    <swiper></swiper>
   </div>
 </template>
 
 <script>
+  import Swiper from '../demo/Swiper'
   export default {
-    name: 'Index'
+    name: 'Index',
+    components: {
+      Swiper
+    }
   }
 </script>
 
